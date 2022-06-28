3d3abd224f56f5d87ffe5659f227f98bf22e89b8
Jaeho Yang <ho4121e@gmail.com>
2022-06-29 06:04:21 +0900

    Feat: modifying title, content function

--- src/Rest.js
+++ src/Rest.js
@@ -3,11 +3,19 @@ import axios from "axios"
 
 const Rest = () => {
   const [text, setText] = useState([]);
+  
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
+  const [inputs, setInputs] = useState({
+    title: '',
+    content: '',
+  });
+  const { title, content } = inputs;
 
   const postHandle = () => {
     axios.post("http://likelion-todo.kro.kr:8000/review/", {
-      title: "제목",
-      content: "내용",
+      title: title,
+      content: content,
     }).then(res => {
       console.log(res);
     }).catch(res => {
@@ -30,11 +38,33 @@ const Rest = () => {
     setText(text.filter(text => text.id !== id));
   }
 
+  
+  // const onChangeTitle = (e) => {
+  //   setTitle(e.target.value);
+  // }
+  // const onChangeContent = (e) => {
+  //   setContent(e.target.value);
+  // }
+      
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  
   return (
     <div>
       <h1>axios 연습</h1>
-      <button onClick={() => {postHandle()}}>POST</button>
-      <button onClick={() => {getHandle()}}>GET</button>
+      <div>
+        <input name="title" placeholder="제목" value={title} onChange={onChange} />
+        <input name="content" placeholder="내용" value={content} onChange={onChange} />
+      </div>
+      <div>
+        <button onClick={() => {postHandle()}}>POST</button>
+        <button onClick={() => {getHandle()}}>GET</button>
+      </div>
       {text.map((e) => (
         <div>
           <span>
